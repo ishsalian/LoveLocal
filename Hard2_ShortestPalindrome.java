fb19031af74d91f1d9cf1f27aeba0f8e6a013b28
VAISHALI <100010595+ishsalian@users.noreply.github.com>
2023-12-04 21:17:19 +0530

    Create Hard2_ShortestPalindrome.java

---
+++ Hard2_ShortestPalindrome.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ShortestPalindrome {
+
+    public static String shortestPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        // Build a new string by concatenating s, a special character, and the reversed s
+        String newStr = s + "#" + new StringBuilder(s).reverse().toString();
+
+        // Compute the KMP prefix array for the new string
+        int[] prefix = new int[newStr.length()];
+        int j = 0;
+
+        for (int i = 1; i < newStr.length(); i++) {
+            while (j > 0 && newStr.charAt(i) != newStr.charAt(j)) {
+                j = prefix[j - 1];
+            }
+
+            if (newStr.charAt(i) == newStr.charAt(j)) {
+                j++;
+            }
+
+            prefix[i] = j;
+        }
+
+        // The remaining part to be added in front of s
+        String remaining = s.substring(prefix[newStr.length() - 1]);
+
+        return new StringBuilder(remaining).reverse().toString() + s;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String userInput = scanner.nextLine();
+
+        String result = shortestPalindrome(userInput);
+        System.out.println("Shortest palindrome: " + result);
+    }
+}
